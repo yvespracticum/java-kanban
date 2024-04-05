cf582c04fb34d4f0b51318bb2a1033c90b08b227
yvespracticum <yvveeessss@ya.ru>
2024-04-05 14:21:04 +0300

    remove commented out code

--- src/service/Managers.java
+++ src/service/Managers.java
@@ -1,20 +1,6 @@
 package service;
 
 public class Managers {
-//    private static TaskManager InMemoryTaskManager;
-//    private static HistoryManager InMemoryHistoryManager;
-//    public static TaskManager getDefault() {
-//        if (InMemoryTaskManager == null) {
-//            return new InMemoryTaskManager();
-//        }
-//        return InMemoryTaskManager;
-//    }
-//    public static HistoryManager getDefaultHistory() {
-//        if (InMemoryHistoryManager == null) {
-//            return new InMemoryHistoryManager();
-//        }
-//        return InMemoryHistoryManager;
-//    }
 
     public static TaskManager getDefault() {
         return new InMemoryTaskManager();
